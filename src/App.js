96ee131bdbf6d4bd5f00260a7584c56fcf757dc1
Ciro Vitiello <ciro.riccione@gmail.com>
2024-02-02 11:40:00 +0100

    first commit no cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import MyHeader from './components/MyHeader'
+import MyFooter from './components/MyFooter'
+import Hero from './components/Hero'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <MyHeader />
       </header>
-    </div>
-  );
+      <main>
+        <Hero />
+      </main>
+      <footer>
+        <MyFooter />
+      </footer>
+    </>
+  )
 }
 
-export default App;
+export default App
